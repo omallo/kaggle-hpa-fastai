1f61e3b76d0475532c85310640e571c3a861e266
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-12-13 10:31:00 +0100

    use the CNN learner factory

--- train.py
+++ train.py
@@ -19,35 +19,31 @@ def load_image_channel(file_path, image_size):
     return channel
 
 
-class ResNet(nn.Module):
-    def __init__(self, type, num_classes):
-        super().__init__()
+def resnet(type, pretrained, num_classes):
+    if type == "resnet18":
+        resnet = resnet18(pretrained=pretrained)
+        num_fc_in_channels = 512
+    elif type == "resnet34":
+        resnet = resnet34(pretrained=pretrained)
+        num_fc_in_channels = 512
+    elif type == "resnet50":
+        resnet = resnet50(pretrained=pretrained)
+        num_fc_in_channels = 2048
+    else:
+        raise Exception("Unsupported resnet model type: '{}".format(type))
 
-        if type == "resnet18":
-            self.resnet = resnet18(pretrained=True)
-            num_fc_in_channels = 512
-        elif type == "resnet34":
-            self.resnet = resnet34(pretrained=True)
-            num_fc_in_channels = 512
-        elif type == "resnet50":
-            self.resnet = resnet50(pretrained=True)
-            num_fc_in_channels = 2048
-        else:
-            raise Exception("Unsupported resnet model type: '{}".format(type))
+    conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3, bias=False)
+    conv1.weight.data[:, 0:3, :, :] = resnet.conv1.weight.data
+    resnet.conv1 = conv1
 
-        conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        conv1.weight.data[:, 0:3, :, :] = self.resnet.conv1.weight.data
-        self.resnet.conv1 = conv1
+    resnet.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
+    resnet.fc = nn.Sequential(
+        nn.BatchNorm1d(num_fc_in_channels),
+        nn.Dropout(0.5),
+        nn.Linear(num_fc_in_channels, num_classes),
+    )
 
-        self.resnet.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
-        self.resnet.fc = nn.Sequential(
-            nn.BatchNorm1d(num_fc_in_channels),
-            nn.Dropout(0.5),
-            nn.Linear(num_fc_in_channels, num_classes),
-        )
-
-    def forward(self, x):
-        return self.resnet(x)
+    return resnet
 
 
 class HpaImageItemList(ImageItemList):
@@ -65,7 +61,11 @@ data = (
 
 data.show_batch(rows=3)
 
-# learner = create_cnn(data, lambda p: ResNet("resnet18", 28), metrics=accuracy)
-learner = Learner(data, ResNet("resnet18", 28), metrics=accuracy)
+learner = create_cnn(
+    data,
+    lambda pretrained: resnet('resnet18', pretrained=pretrained, num_classes=28),
+    metrics=accuracy)
+
+# learner = Learner(data, ResNet("resnet18", 28), metrics=accuracy)
 
 learner.fit(1)
