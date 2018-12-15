f4fff9117ce4cc1af235071ee6c6794c00ff78e7
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-12-15 14:34:19 +0100

    save the best model

--- train.py
+++ train.py
@@ -160,13 +160,15 @@ if True:
 else:
     learner = Learner(data, create_senet('seresnext50', num_classes=28))
 
+learner.path = Path(output_dir)
 learner.loss_func = focal_loss
 learner.metrics = [F1Score()]
-learner.callbacks = [EarlyStoppingCallback(learner, monitor='f1_score', patience=5, min_delta=1e-3)]
-# learner.path = Path(output_dir)
-# learner.to_fp16()
+learner.callbacks = [
+    EarlyStoppingCallback(learner, monitor='f1_score', patience=5, min_delta=1e-3),
+    SaveModelCallback(monitor='f1_score')
+]
 
-# print(learn.summary)
+# print(learner.summary)
 
 learner.fit(1)
 learner.unfreeze()
