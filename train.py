13eea6b05bde2814134467322ecf59ce44c092ef
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-12-15 14:36:49 +0100

    specify the metric tracker mode

--- train.py
+++ train.py
@@ -164,8 +164,8 @@ learner.path = Path(output_dir)
 learner.loss_func = focal_loss
 learner.metrics = [F1Score()]
 learner.callbacks = [
-    EarlyStoppingCallback(learner, monitor='f1_score', patience=5, min_delta=1e-3),
-    SaveModelCallback(monitor='f1_score')
+    EarlyStoppingCallback(learner, monitor='f1_score', mode='max', patience=5, min_delta=1e-3),
+    SaveModelCallback(monitor='f1_score', mode='max')
 ]
 
 # print(learner.summary)
