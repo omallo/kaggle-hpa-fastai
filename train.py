2905d6e164143178a549c955a5518c0a5357f446
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-12-18 00:10:20 +0100

    enable TTA prediction

--- train.py
+++ train.py
@@ -199,10 +199,11 @@ test_prediction_categories = calculate_categories(test_prediction_logits, best_t
 write_submission(test_prediction_categories, '{}/submission.csv'.format(output_dir))
 np.save('{}/test_prediction_logits.npy'.format(output_dir), test_prediction_logits.cpu().data.numpy())
 
-# valid_prediction_logits, valid_prediction_categories_one_hot = learn.TTA(ds_type=DatasetType.Valid)
-# best_threshold, best_score, _ = calculate_best_threshold(valid_prediction_logits, valid_prediction_categories_one_hot)
-# print('best threshold / score: {:.3f} / {:.3f}'.format(best_threshold, best_score))
+valid_prediction_logits, valid_prediction_categories_one_hot = learn.TTA(ds_type=DatasetType.Valid)
+best_threshold, best_score, _ = calculate_best_threshold(valid_prediction_logits, valid_prediction_categories_one_hot)
+print('best threshold / score: {:.3f} / {:.3f}'.format(best_threshold, best_score))
 
-# test_prediction_logits, _ = learn.TTA(ds_type=DatasetType.Test)
-# test_prediction_categories = calculate_categories(test_prediction_logits, best_threshold)
-# write_submission(test_prediction_categories, '{}/submission_tta.csv'.format(output_dir))
+test_prediction_logits, _ = learn.TTA(ds_type=DatasetType.Test)
+test_prediction_categories = calculate_categories(test_prediction_logits, best_threshold)
+write_submission(test_prediction_categories, '{}/submission_tta.csv'.format(output_dir))
+np.save('{}/test_prediction_logits_tta.npy'.format(output_dir), test_prediction_logits.cpu().data.numpy())
