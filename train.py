82a18413fe1b19e105b8750901cb5a01f7b87248
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-12-17 21:18:28 +0100

    train for less epochs

--- train.py
+++ train.py
@@ -181,7 +181,7 @@ learn.callbacks = [
 lr = 4e-2
 learn.fit(1, lr=lr)
 learn.unfreeze()
-learn.fit_one_cycle(50, max_lr=learn.lr_range(slice(lr)))
+learn.fit_one_cycle(20, max_lr=learn.lr_range(slice(lr)))
 
 learn.load('model_best_f1')
 
