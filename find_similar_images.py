3efabca6687b5a20aedc0fba5c4e9fc9887c9e93
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-12-26 13:23:10 +0100

    cleanup

--- find_similar_images.py
+++ find_similar_images.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 from __future__ import (absolute_import, division, print_function)
-from PIL import Image
-import six
 
 import imagehash
+from PIL import Image
 
 """
 Demo of hashing
